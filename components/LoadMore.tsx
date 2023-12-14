f8a4813fab2b0079c3dcc23f56d39058d7d7c08c
CodeShadowing95 <ksenbonsakura@gmail.com>
2023-12-14 12:19:02 +0100

    FIX: Image Banner

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,43 @@
+"use client";
+
+
+import { fetchAnime } from "@/app/action";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import AnimeCard from "./AnimeCard";
+
+let page = 2;
+
+export type AnimeCard = JSX.Element;
 
 function LoadMore() {
+  const [ref, inView] = useInView();
+  const [data, setData] = useState<AnimeCard[]>([]);
+
+  // ************* Infinite scroll *************
+  useEffect(() => {
+    // alert("Load more...");
+    if (inView) {
+      fetchAnime(page)
+      .then((res) => {
+        // To keep track of all datas we had before
+        setData([...data, ...res]);
+        page++;
+      })
+    }
+  }, [inView, data]);
+  
+
   return (
     <>
+      {/* To load all the subsequent pages from page 1 */}
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data}
+      </section>
+
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
